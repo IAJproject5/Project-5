d92254d7ff84fbefcc29f6d5353f7cad290f0fb3
Jackson Isenhower <jacksonisenhower@cedarville.edu>
2023-04-10 21:05:49 -0400

    Added UserManagement View and Controller, as well as AppUser model.

---
+++ Controllers/UserManagementController.cs
@@ -0,0 +1,24 @@
+﻿using Project_5.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Project_5.Areas.Identity.Data;
+
+namespace Project_5.Controllers
+{
+    //[Authorize(Roles = "Admin")]
+    public class UserManagementController : Controller
+    {
+        //private readonly UserManager<AppUser> userManager;
+
+        //public UserManagementController(UserManager<AppUser> usrMgr)
+        //{
+        //    userManager = usrMgr;
+        //}
+
+        public IActionResult Index()
+        {
+            return View(/*userManager.Users*/);
+        }
+    }
+}
---
+++ Models/AppUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Project_5.Models
+{
+    public class AppUser : IdentityUser
+    {
+    }
+}
