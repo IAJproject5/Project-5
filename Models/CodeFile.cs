3ab40102be2803a4230d703662ea348aafb41249
8mcgraw <123113246+8mcgraw@users.noreply.github.com>
2023-04-11 03:49:47 -0400

    new code file

---
+++ Models/CodeFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Project_5.Project_5.Models;
+
+internal class Test
+{
+	private static void Main()
+	{
+		using (var context = new Project_5Context())
+		{
+			var tabs = context.IajPlans.ToList();
+		}
+	}
+}
